23a764b304b6334df1a31ff7fa6809814dc2ec08
HWaruguru <hannah.waruguru8@gmasil.com>
2021-09-30 01:22:46 +0300

    Add all routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { StatsComponent } from './stats/stats.component';
 import { AuthComponent } from './auth/auth.component';
 import { PostFormComponent } from './post-form/post-form.component';
 import { PostComponent } from './post/post.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
   {path: 'covid-stats', component: StatsComponent},
   {path: 'login', component: AuthComponent},
   {path: 'register', component: AuthComponent},
-  {path: 'post', component: PostComponent},
-  {path: 'add-post', component: PostFormComponent},
+  {path: 'post/:id', component: PostComponent, canActivate: [AuthGuard]},
+  {path: 'add-post', component: PostFormComponent, canActivate: [AuthGuard]},
   {path: '', redirectTo: 'covid-stats', pathMatch: 'full'},
   {path: '**', redirectTo: 'covid-stats', pathMatch: 'full'}
 ];
