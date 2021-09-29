c7fa0e612146bfb6634ee88cef1a602a694a9764
HWaruguru <hannah.waruguru8@gmasil.com>
2021-09-29 12:49:09 +0300

    Created a routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { StatsComponent } from './stats/stats.component';
+import { AuthComponent } from './auth/auth.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'covid-stats', component: StatsComponent},
+  {path: 'login', component: AuthComponent},
+  {path: 'register', component: AuthComponent},
+  {path: 'post', component: PostComponent},
+  {path: 'add-post', component: PostFormComponent},
+  {path: '', redirectTo: 'covid-stats', pathMatch: 'full'},
+  {path: '**', redirectTo: 'covid-stats', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
