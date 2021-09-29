5f18041799ed4c34138cf3818908f102951d7d17
HWaruguru <hannah.waruguru8@gmasil.com>
2021-09-30 01:34:37 +0300

    Add posts from backend

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../covid-service/auth.service';
 import { Post } from '../post';
 
 @Component({
@@ -8,18 +10,19 @@ import { Post } from '../post';
 })
 export class StatsComponent implements OnInit {
   title = 'Countries';
-  posts: Post[] = [
-    new Post(1, 'Kenya', 120, 30, 40, 50, new Date(2021, 7, 20)),
-    new Post(2, 'Uganda', 130, 60, 7, 8, new Date(2021, 7, 20)),
-    new Post(3,  'South Africa', 5, 43, 52, 17, new Date(2021, 7, 20)),
-    new Post(4,  'Haiti', 10, 34, 56, 79, new Date(2021, 7, 20)),
-    new Post(5,  'India', 102, 76, 41, 21, new Date(2021, 7, 20)),
-    new Post(6, 'Norway', 170, 35, 75, 80, new Date(2021, 7, 20))
-  ];
+  posts: Post[];
+  currentUser: any;
 
-  constructor() { }
+  constructor(private postService: AuthService, private router: Router) { 
+    this.postService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  async getPosts() {
+    this.posts = await this.postService.fetchPosts();
+  }
 
   ngOnInit(): void {
+    this.getPosts()
   }
 
 }
