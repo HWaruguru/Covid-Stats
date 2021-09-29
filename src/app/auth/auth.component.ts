655daedad5bde97bf42c961b33feb8a8ecd17d71
HWaruguru <hannah.waruguru8@gmasil.com>
2021-09-30 01:27:40 +0300

    Add signup and sign in

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../covid-service/auth.service';
 
 @Component({
   selector: 'auth-page',
@@ -11,11 +12,11 @@ export class AuthComponent implements OnInit {
   title: String = '';
   isSubmitting: boolean = false;
   authForm: FormGroup;
+  authenticated: boolean = false;
+  error: string = "";
+  returnUrl: string = "";
 
-  constructor(
-    private route: ActivatedRoute,
-    private fb: FormBuilder
-  ) {
+  constructor(private activatedRoute: ActivatedRoute, private route: Router, private fb: FormBuilder, private authService: AuthService) {
     this.authForm = this.fb.group({
       'email': ['', Validators.required],
       'password': ['', Validators.required]
@@ -23,19 +24,49 @@ export class AuthComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.route.url.subscribe(data => {
+    this.activatedRoute.url.subscribe(data => {
       this.authType = data[data.length - 1].path;
       this.title = (this.authType === 'login') ? 'Sign In' : 'Sign Up';
       if (this.authType === 'register') {
         this.authForm.addControl('username', new FormControl('', Validators.required));
       }
+      this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/covid-stats';
     });
   }
 
-  submitForm() {
-    this.isSubmitting = true;
+  async registerUser(username: string, email: string, password: string) {
+    let res: any = await this.authService.registerUser(username, email, password)
+    if (res.registered) {
+      this.route.navigate(['/login'])
+    }
+    else {
+      this.error = res.error
+      this.isSubmitting = false;
+      this.route.navigate(['/register'])
+    }
+  }
 
+  async loginUser(email: string, password: string) {
+    let res: any = await this.authService.login(email, password)
+    console.log(res)
+    if (res.authenticated) {
+      this.route.navigate([this.returnUrl])
+    }
+    else {
+      this.error = res
+      this.isSubmitting = false;
+      this.route.navigate(['/login'])
+    }
+  }
+
+  async submitForm() {
+    this.isSubmitting = true;
     let credentials = this.authForm.value;
-    console.log(credentials);
+    if (this.authType == 'login') {
+      this.loginUser(credentials.email, credentials.password)
+      } else if(this.authType === 'register') {
+        this.registerUser(credentials.username, credentials.email, credentials.password)
+      }
+    
   }
 }
